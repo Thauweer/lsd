fc0f4874baf3b7ba922910a92b5f5ae7480512cf
Thauweer <thauweer@yandex.ru>
2022-10-21 21:08:03 +0400

    init

---
+++ islands/Lsd.tsx
@@ -0,0 +1,21 @@
+type Props = {}
+
+const Lsd = (props: Props) => {
+    const handleClick = () => {
+        window.location.href = 'https://discord.gg/3KntQcYy2z'
+    }
+    return (
+        <div
+            class="flex text-purple-300 hover:text-purple-600 active:text-purple-800 visited:text-blue-600 cursor-pointer text-5xl img:w-24 sm:text-6xl md:text-7xl lg:text-8xl xl:text-10xl "
+            onclick={handleClick}>
+            <div class="flex flex-row items-center p-20 bg-gray-800 rounded-2xl">
+                <h1 class="animate-pulse">
+                    LSD Server
+                </h1>
+                <img src="https://cdn.discordapp.com/icons/422325987481747457/913f45a38e006f83628577e669456c57.webp?size=480" alt="lsd" class="rounded-full inline ml-5 w-20 sm:w-24 md:w-28 lg:w-32 xl:w-36 animate-pulse" />
+            </div>
+        </div>
+    )
+}
+
+export default Lsd
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,22 +1,15 @@
-// import { Head } from "$fresh/runtime.ts";
-// import Counter from "../islands/Counter.tsx";
-
+import Lsd from "../islands/Lsd.tsx"
 const Head = () => (
   <head>
-      <title>LSD server</title>
+    <title>LSD server</title>
   </head>
 );
 
 export default function Home() {
   return (
-    <div class="flex justify-center items-center h-screen text-6xl">
-      <Head/>
-      <div>
-        <h1>
-          LSD Server
-        </h1>
-        <a href="https://discord.gg/3KntQcYy2z" class="text-purple-300 hover:text-purple-600 active:text-purple-800 visited:text-blue-600">here</a>
-      </div>
+    <div class="flex justify-center items-center h-screen bg-gradient-to-t from-red-700 via-blue-700 to-white-700">
+      <Head />      
+      <Lsd/>
     </div>
   );
 }
